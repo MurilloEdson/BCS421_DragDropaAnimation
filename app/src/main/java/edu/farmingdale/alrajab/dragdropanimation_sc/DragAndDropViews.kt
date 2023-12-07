738e9be3581b34b307f4685444bd5e027e7b8148
MurilloEdson <murilloedsonj@gmail.com>
2023-12-06 23:27:09 -0500

    Drap and Drop enabled along with animation

--- app/src/main/java/edu/farmingdale/alrajab/dragdropanimation_sc/DragAndDropViews.kt
+++ app/src/main/java/edu/farmingdale/alrajab/dragdropanimation_sc/DragAndDropViews.kt
@@ -1,17 +1,14 @@
 package edu.farmingdale.alrajab.dragdropanimation_sc
 
-import android.content.ClipData
-import android.content.ClipDescription
-import android.graphics.Canvas
-import android.graphics.Point
+import android.content.*
+import android.graphics.*
+import android.graphics.drawable.AnimationDrawable
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.DragEvent
-import android.view.View
-import android.widget.Button
-import android.widget.ImageView
+import android.view.*
+import android.widget.*
 import edu.farmingdale.alrajab.dragdropanimation_sc.databinding.ActivityDragAndDropViewsBinding
 
 class DragAndDropViews : AppCompatActivity() {
@@ -23,14 +20,35 @@ class DragAndDropViews : AppCompatActivity() {
         setContentView(binding.root)
         binding.holder01.setOnDragListener(arrowDragListener)
         binding.holder02.setOnDragListener(arrowDragListener)
-
+        binding.holder03.setOnDragListener(arrowDragListener)
+        binding.holder04.setOnDragListener(arrowDragListener)
+        binding.holder05.setOnDragListener(arrowDragListener)
 
         binding.upMoveBtn.setOnLongClickListener(onLongClickListener)
+        binding.downMoveBtn.setOnLongClickListener(onLongClickListener)
+        binding.backMoveBtn.setOnLongClickListener(onLongClickListener)
+        binding.forwardMoveBtn.setOnLongClickListener(onLongClickListener)
 
+        val rocketImage = findViewById<ImageView>(R.id.rocket)
+        rocketImage.setImageResource(R.drawable.rocket_flying)
+        val rocketAnimation = rocketImage.drawable as AnimationDrawable
 
+        binding.start.setOnClickListener {
 
+            if (rocketAnimation.isRunning) {
+                rocketAnimation.stop()
+            } else {
+                rocketAnimation.start()
+            }
+        }
+        binding.reset.setOnClickListener {
+            binding.holder01.setImageResource(R.drawable.border)
+            binding.holder02.setImageResource(R.drawable.border)
+            binding.holder03.setImageResource(R.drawable.border)
+            binding.holder04.setImageResource(R.drawable.border)
+            binding.holder05.setImageResource(R.drawable.border)
+        }
     }
-
 
 
     private val onLongClickListener = View.OnLongClickListener { view: View ->
@@ -47,7 +65,6 @@ class DragAndDropViews : AppCompatActivity() {
             } else {
                 it.startDrag(dragData, myShadow, null, 0)
             }
-
             true
         }
         false
@@ -63,9 +80,11 @@ class DragAndDropViews : AppCompatActivity() {
                     return@OnDragListener true
                 }
                 DragEvent.ACTION_DRAG_ENTERED -> {
+                    view.setBackgroundResource(R.drawable.hl_border)
                     return@OnDragListener true
                 }
                 DragEvent.ACTION_DRAG_EXITED-> {
+                    view.setBackgroundResource(R.drawable.border)
                     return@OnDragListener true
                 }
                 // No need to handle this for our use case.
@@ -75,11 +94,15 @@ class DragAndDropViews : AppCompatActivity() {
 
                 DragEvent.ACTION_DROP -> {
                     // Read color data from the clip data and apply it to the card view background.
+                    view.setBackgroundResource(R.drawable.border)
                     val item: ClipData.Item = dragEvent.clipData.getItemAt(0)
                     val lbl = item.text.toString()
                     Log.d("BCCCCCCCCCCC", "NOTHING > >  " + lbl)
                    when(lbl.toString()){
                        "UP"->view.setImageResource( R.drawable.ic_baseline_arrow_upward_24)
+                       "DOWN"->view.setImageResource( R.drawable.ic_baseline_arrow_downward_24)
+                       "FORWARD"->view.setImageResource( R.drawable.ic_baseline_arrow_forward_24)
+                       "BACK"->view.setImageResource( R.drawable.ic_baseline_arrow_back_24)
                    }
                     return@OnDragListener true
                 }
